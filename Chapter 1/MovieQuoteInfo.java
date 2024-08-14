d9fbf23e18d4bc81fc4b1bb32d37535603dace67
R4CUL <146191199+R4CUL@users.noreply.github.com>
2024-08-15 00:46:30 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+public class MovieQuoteInfo
+{
+	public static void main(String[] args) {
+	    
+	    String movieQuote = "'I figure life's a gift and I don't intend on wasting it.'";
+	    String movieFrom  = "Produced by 20th Century Fox and Paramount Pictures";
+	    String movieCharacter = "It’s said by the character Jack Dawson, played by Leonardo DiCaprio";
+	    String movieYear = "Titanic (1997)";
+	    
+		System.out.print("\n" + movieQuote + "\n" + movieFrom +"\n" + movieCharacter + "\n" + movieYear);
+	}
+}
