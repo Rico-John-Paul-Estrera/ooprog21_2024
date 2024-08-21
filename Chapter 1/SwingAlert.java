197de294f94d4eec713f01689ab6fddceab64aaf
Rico Estrera <146191199+Rico-John-Paul-Estrera@users.noreply.github.com>
2024-08-21 23:11:44 +0800

    Add files via upload

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class SwingAlert
+
+{
+    public static void main(String[] args) 
+  {
+      JOptionPane.showMessageDialog(null, "Hello World!");
+  }
+}  
+
+
+  
+  
