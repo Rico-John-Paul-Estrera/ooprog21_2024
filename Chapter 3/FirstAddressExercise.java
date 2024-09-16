80d9acb1f7787866d4bce459cc4ca8a63773f260
Rico Estrera <146191199+Rico-John-Paul-Estrera@users.noreply.github.com>
2024-09-16 23:39:29 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,14 @@
+public class FirstAddressExercise {
+    public static void displayAddress() {
+        System.out.println("Samsung Electronics");
+        System.out.println("129 Samsung-ro");
+        System.out.println("Yeongtong District, Suwon, Gyeonggi Province, South Korea");
+        System.out.println("Company: Samsung Electronics Co., Ltd.");
+        
+        
+    }
+    public static void main(String[] args){
+        displayAddress();
+     // System.out.println("First Java application");
+    }
+}
