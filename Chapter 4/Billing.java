323871f10d1276ff77d90c125e8c55d1d224397a
Rico Estrera <146191199+Rico-John-Paul-Estrera@users.noreply.github.com>
2024-10-02 01:28:59 +0800

    Update Billing.java

--- Chapter 4/Billing.java
+++ Chapter 4/Billing.java
@@ -1,31 +1,36 @@
-public class Billing {
+class Billing {
 
-    public static void computeBill(double price) {
+     static void computeBill(double price) {
         double tax = 0.08 * price;
         double total = price + tax;
-        System.out.println("Total bill (one item): $" + total);
+        System.out.println("The total price for the bill is: $" + total);
     }
 
-    public static void computeBill(double price, double quantity) {
+     static void computeBill(double price, int quantity) {
         double subtotal = price * quantity;
-        double tax = 0.08 * subtotal;
+        double tax = subtotal * 0.08 ;
         double total = subtotal + tax;
-        System.out.println("Total bill (multiple items): $" + total);
+        System.out.println("The total price for the bill is: $" + total);
     }
 
-    public static void computeBill(double price, double quantity, double couponDiscount) {
+     static void computeBill(double price, int quantity, int couponDiscount) {
         double subtotal = price * quantity;
         double discount = (couponDiscount / 100.0) * subtotal; 
         double discountedPrice = subtotal - discount;
         double tax = 0.08 * discountedPrice; 
         double total = discountedPrice + tax;
-        System.out.println("Total bill (with discount): $" + total);
+        System.out.println("The total price for the bill is: $" + total);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] a) {
 
         computeBill(23.2);  
-        computeBill(48.8);  
-        computeBill(70.9);  
+        computeBill(48.8, 28);  
+        computeBill(70.9, 28, 28);
+        
+        /* computeBill(23.2);    output: 25.055999999999997                            
+        computeBill(48.8);    output: 52.70399999999999 
+        computeBill(70.9);    output: 76.572 */
+          
     }
 }
