c88b47318b72533cdec4aeeb4ecba523833c563d
Rico Estrera <146191199+Rico-John-Paul-Estrera@users.noreply.github.com>
2024-10-02 09:05:50 +0800

    Update MathTest.java

--- Chapter 4/MathTest.java
+++ Chapter 4/MathTest.java
@@ -1,11 +1,11 @@
 public class MathTest {
 
    public static void main (String[] a) {
-    System.out.println(Math.sqrt(0));
-    System.out.println(Math.sqrt(1));
-    System.out.println(Math.sqrt(9));
-    System.out.println(Math.sqrt(0.64));
-    System.out.println(Math.sqrt(-9));
+    System.out.println("SQRT: " + Math.sqrt(0));
+    System.out.println("SQRT: " + Math.sqrt(1));
+    System.out.println("SQRT: " + Math.sqrt(9));
+    System.out.println("SQRT: " + Math.sqrt(0.64));
+    System.out.println("SQRT: " + Math.sqrt(-9));
    }
 }   
    
