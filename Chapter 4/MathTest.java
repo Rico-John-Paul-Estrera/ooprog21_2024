25fa30bee3a584fa497fc2073042c1c171d39d77
Rico Estrera <146191199+Rico-John-Paul-Estrera@users.noreply.github.com>
2024-10-02 09:04:23 +0800

    Add files via upload

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,12 @@
+public class MathTest {
+
+   public static void main (String[] a) {
+    System.out.println(Math.sqrt(0));
+    System.out.println(Math.sqrt(1));
+    System.out.println(Math.sqrt(9));
+    System.out.println(Math.sqrt(0.64));
+    System.out.println(Math.sqrt(-9));
+   }
+}   
+   
+   
