cf8d12c34753bf1e352645c22eed83c5b42589a8
Rico Estrera <146191199+Rico-John-Paul-Estrera@users.noreply.github.com>
2024-10-02 09:12:10 +0800

    Add files via upload

--- Chapter 4/MathTest.java
+++ Chapter 4/MathTest.java
@@ -1,12 +1,30 @@
 public class MathTest {
-
-   public static void main (String[] a) {
-    System.out.println("SQRT: " + Math.sqrt(0));
-    System.out.println("SQRT: " + Math.sqrt(1));
-    System.out.println("SQRT: " + Math.sqrt(9));
-    System.out.println("SQRT: " + Math.sqrt(0.64));
-    System.out.println("SQRT: " + Math.sqrt(-9));
-   }
-}   
-   
-   
+    public static void main(String[] args) {
+        double number = 37.5;
+        
+        double sqrt = Math.sqrt(number);
+        System.out.println("SQRT: " + sqrt);
+        
+        double angle = Math.toRadians(45);
+        double sinValue = Math.sin(angle);
+        double cosValue = Math.cos(angle);
+        System.out.println("SIN: -" + sinValue);
+        System.out.println("COS: -" + cosValue);
+        
+        double floorValue = Math.floor(number);
+        double ceilValue = Math.ceil(number);
+        System.out.println("FLOOR: " + floorValue);
+        System.out.println("CEIL: " + ceilValue);
+        
+        long roundValue = Math.round(number);
+        System.out.println("ROUND: " + roundValue);
+        
+        int maxValue = Math.max(71, 68);
+        int minValue = Math.min(71, 68);
+        System.out.println("MAX: " + maxValue);
+        System.out.println("MIN: " + minValue);
+        
+        double randomValue = Math.random() * 100; 
+        System.out.println("RANDOM: " + randomValue);
+    }
+}
